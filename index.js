ef877c7effda48aace82b1315fb214fe216b545d
Akshay Kamble <77927918+gadgetfather@users.noreply.github.com>
2021-09-29 06:09:52 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,49 @@
+var readlineSync = require('readline-sync');
+
+var data = [
+{name:"",
+ prefinal:"",
+ final:""
+  },
+  {name:"",
+  prefinal:"",
+  final:""
+  },
+  {name:"",
+  prefinal:"",
+  final:""
+  },
+  {name:"",
+ prefinal:"",
+ final:""
+  },
+  {name:"",
+  prefinal:"",
+  final:""
+  }
+]
+var arrayOfMarks = []
+var sumOfMarks = []
+
+sum = 0
+for (i= 0;i<2;i++){
+  console.log(`student ${i}`)
+  var name = readlineSync.question("what is your name? ")
+  data[i].name =name
+  var prefinal = readlineSync.question("what are your pre-final marks? ")
+  
+  data[i].prefinal = prefinal
+  var premarks  = Number(prefinal)
+  var final = readlineSync.question("what are your final marks? ")
+  data[i].final = final
+  var finalMark = Number(final)
+  arrayOfMarks.push(data[i].final)
+  var sum = premarks + finalMark
+  sumOfMarks.push(sum)
+
+ 
+  
+}
+console.log(`Highest marks are ${Math.max(...arrayOfMarks)}`)
+const reducer = (previousValue, currentValue) => previousValue + currentValue;
+console.log(`Average of 5 students is ${sumOfMarks.reduce(reducer)/5}`)
